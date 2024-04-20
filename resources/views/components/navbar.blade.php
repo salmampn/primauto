ee8416804d15548d288ba22b89e46b7aa0a48d73
salmampn <salmamanda03@gmail.com>
2024-04-20 17:42:34 +0700

    Build Homepage

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,20 @@
-<nav id="navbar" class="flex justify-between pb-1 text-white fixed top-0 left-0 right-0 z-10">
-    <div class="absolute inset-0 -z-10">
+<nav id="navbar" class="flex justify-between pb-1 bg-white fixed top-0 left-0 right-0 z-10">
+    {{-- <div class="absolute inset-0 -z-10">
         <div class="absolute inset-0 bg-black opacity-70"></div>
-    </div>
+    </div> --}}
     <div class="flex ml-10 gap-2">
-        <img class="w-28" src="{{ asset('storage/logo.png') }}" alt="Primauto Logo">
+        <img class="w-20" src="{{ asset('storage/logo.png') }}" alt="Primauto Logo">
         <div class="m-auto">
             <i class="flex text-2xl font-bold m-auto">
                 <h1>Prim</h1>
                 <h1 class="text-red-500">auto</h1>
             </i>
-            <p class="text-center">Used Car</p>
+            <p class="text-center font-semibold">Used Car</p>
         </div>
     </div>
     <div class="flex gap-x-20 mr-10 text-xl font-semibold">
-        <div class="m-auto hover:text-red-500"><a href="#home">Home</a></div>
-        <div class="m-auto hover:text-red-500"><a href="#products">Products</a></div>
-        <div class="m-auto hover:text-red-500"><a href="#contact">Contact</a></div>
+        <div class="m-auto hover:text-red-500"><a href="">Home</a></div>
+        <div class="m-auto hover:text-red-500"><a href="">Products</a></div>
+        <div class="m-auto hover:text-red-500"><a href="">Contact</a></div>
     </div>
 </nav>
